392854616c8f583f5554789061d20ad2cae29301
anton <anton.cobzew@yandex.ru>
2019-03-14 11:17:15 +0300

    Task 2

--- 2/main.c
+++ 2/main.c
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include <assert.h>
 
+
 #define STACK_INITIALIZER {NULL, 0, 0};
 #define STACK_START_CAPACITY 10
 #define STACK_CAPACITY_MULTIPLIER 2
@@ -16,8 +17,10 @@ typedef struct {
 } stack;
 
 bool stack_push(stack *stack, char *value);
+bool stack_push_char(stack *stack, char value);
 char *stack_top(stack *stack);
 char *stack_before_top(stack *stack);
+char stack_top_char(stack *stack);
 void stack_pop(stack *stack);
 bool stack_grow(stack *stack);
 bool is_stack_empty(const stack *stack);
@@ -36,48 +39,94 @@ typedef struct {
 
 void string_clear(string *string);
 bool string_add(string *string, char value);
+bool string_append(string *string, const char *other);
 bool string_grow(string *string);
 bool string_grow_to(string *string, size_t new_capacity);
+bool is_string_empty(string *string);
 void string_free(string *string);
 
-void print_error(); // todo
 
-size_t get_operator_precedence(char operator);
-
-void calculate_on_stacks(stack *operators_stack, stack *operands_stack);
+bool evaluate_expression(string *expression, string *result);
+void calculate_expression_on_stack(stack *operators_stack, stack *operands_stack);
 void calculate(const char *first_operand, const char *second_operand, char operator, string *result);
 void add(const char *first_operand, const char *second_operand, string *result);
 void subtract(const char *first_operand, const char *second_operand, string *result);
 void multiply(const char *first_operand, const char *second_operand, string *result);
 void divide(const char *first_operand, const char *second_operand, string *result);
+size_t get_operator_precedence(char operator);
+
+void print_error();
+
+char *strdup(const char *s);
 
 int main() {
-  stack operators_stack = STACK_INITIALIZER;
-  stack operands_stack = STACK_INITIALIZER;
-  string input_number = STRING_INITIALIZER;
-  char c;
-  while (c = (char) getchar(), c != EOF) {
-    if (c == '+' || c == '-' || c == '*' || c == '/') {
-      stack_push(&operands_stack, input_number.content);
-      string_clear(&input_number);
-      calculate_on_stacks(&operators_stack, &operands_stack);
-      char operator_as_string[2] = "\0";
-      operator_as_string[0] = c;
-      stack_push(&operators_stack, operator_as_string);
-    } else if (isdigit(c)) {
-      string_add(&input_number, c);
-    }
-  }
+  string expression = STRING_INITIALIZER;
+  char input_buffer[1024];
+  while (fgets(input_buffer, 1024, stdin) != NULL)
+    string_append(&expression, input_buffer);
+  string result = STRING_INITIALIZER;
+  if (evaluate_expression(&expression, &result))
+    printf("%s\n", result.content);
+  else
+    print_error();
 
-  stack_push(&operands_stack, input_number.content);
-  calculate_on_stacks(&operators_stack, &operands_stack);
-  printf("%s", stack_top(&operands_stack));
-
-  stack_free(&operators_stack);
-  stack_free(&operands_stack);
-  string_free(&input_number);
+  string_free(&expression);
+  string_free(&result);
 
   return 0;
+}
+
+bool evaluate_expression(string *expression, string *result) {
+  stack operators = STACK_INITIALIZER;
+  stack operands = STACK_INITIALIZER;
+  string number = STRING_INITIALIZER;
+  char current;
+  size_t i = 0;
+  bool success = true;
+  while (current = expression->content[i++], current != '\0' && success) {
+    if (isdigit(current)) {
+      string_add(&number, current);
+    } else if (current == '+' || current == '-' || current == '*' || current == '/') {
+      if (!is_string_empty(&number)) {
+        stack_push(&operands, number.content);
+        string_clear(&number);
+      }
+      if (is_stack_empty(&operators) || stack_top_char(&operators) == '('
+          || get_operator_precedence(current) > get_operator_precedence(stack_top_char(&operators))) {
+        stack_push_char(&operators, current);
+      } else {
+        while (!is_stack_empty(&operators) && stack_top_char(&operators) != '('
+            && get_operator_precedence(stack_top_char(&operators)) >= get_operator_precedence(current))
+          calculate_expression_on_stack(&operators, &operands);
+        stack_push_char(&operators, current);
+      }
+    } else if (current == '(') {
+      stack_push_char(&operators, current);
+    } else if (current == ')') {
+      if (!is_string_empty(&number)) {
+        stack_push(&operands, number.content);
+        string_clear(&number);
+      }
+      while (!is_stack_empty(&operators) && stack_top_char(&operators) != '(')
+        calculate_expression_on_stack(&operators, &operands);
+      if (is_stack_empty(&operators))
+        success = false;
+      else
+        stack_pop(&operators);
+    }
+  }
+  if (success) {
+    if (!is_string_empty(&number))
+      stack_push(&operands, number.content);
+    while (!is_stack_empty(&operators))
+      calculate_expression_on_stack(&operators, &operands);
+    string_append(result, stack_top(&operands));
+  }
+
+  stack_free(&operators);
+  stack_free(&operands);
+  string_free(&number);
+  return success;
 }
 
 bool stack_push(stack *stack, char *value) {
@@ -88,9 +137,19 @@ bool stack_push(stack *stack, char *value) {
   return true;
 }
 
+bool stack_push_char(stack *stack, char value) {
+  char value_as_string[2] = "\0";
+  value_as_string[0] = value;
+  return stack_push(stack, value_as_string);
+}
+
 char *stack_top(stack *stack) {
   assert(stack != NULL && stack->size > 0);
   return stack->values[stack->size - 1];
+}
+
+char stack_top_char(stack *stack) {
+  return stack_top(stack)[0];
 }
 
 char *stack_before_top(stack *stack) {
@@ -122,9 +181,9 @@ bool is_stack_empty(const stack *stack) {
 
 void stack_free(stack *stack) {
   assert(stack != NULL && stack->values != NULL);
-  free(stack->values);
   while (stack->size > 0)
     free(stack->values[--stack->size]);
+  free(stack->values);
   stack->capacity = 0;
 }
 
@@ -139,6 +198,20 @@ bool string_add(string *string, char value) {
   if (string->size + 1 >= string->capacity && !string_grow(string))
     return false;
   string->content[string->size++] = value;
+  string->content[string->size] = '\0';
+  return true;
+}
+
+bool string_append(string *string, const char *other) {
+  assert(string != NULL && other != NULL);
+  size_t other_length = strlen(other);
+  if (string->size + other_length >= string->capacity) {
+    size_t new_capacity = string->capacity +
+        other_length * (1 + STRING_CAPACITY_MULTIPLIER) / STRING_CAPACITY_MULTIPLIER;
+    if (!string_grow_to(string, new_capacity)) return false;
+  }
+  strncpy(string->content + string->size, other, other_length);
+  string->size += other_length;
   string->content[string->size] = '\0';
   return true;
 }
@@ -160,10 +233,14 @@ bool string_grow_to(string *string, size_t new_capacity) {
   return true;
 }
 
+bool is_string_empty(string *string) {
+  assert(string != NULL);
+  return string->size == 0;
+}
+
 void string_free(string *string) {
   assert(string != NULL);
-  if (string->content != NULL)
-    free(string->content);
+  free(string->content);
   string->size = string->capacity = 0;
 }
 
@@ -173,31 +250,24 @@ size_t get_operator_precedence(char operator) {
   switch (operator) {
     case '+':
     case '-':return 1;
-
     case '*':
     case '/':return 2;
-
-    case '^':return 3;
     default:return 0;
   }
 }
 
-void calculate_on_stacks(stack *operators_stack, stack *operands_stack) {
+void calculate_expression_on_stack(stack *operators_stack, stack *operands_stack) {
   string result = STRING_INITIALIZER;
-  while (!is_stack_empty(operators_stack)) {
-    char *operator = stack_top(operators_stack);
-    char *second_operand = stack_top(operands_stack);
-    char *first_operand = stack_before_top(operands_stack);
+  char operator = stack_top_char(operators_stack);
+  char *second_operand = stack_top(operands_stack);
+  char *first_operand = stack_before_top(operands_stack);
 
-    calculate(first_operand, second_operand, operator[0], &result);
-    printf("CALC %s %s %s = %s\n", first_operand, operator, second_operand, result.content);
+  calculate(first_operand, second_operand, operator, &result);
 
-    stack_pop(operators_stack);
-    stack_pop(operands_stack);
-    stack_pop(operands_stack);
-    stack_push(operands_stack, result.content);
-    string_clear(&result);
-  }
+  stack_pop(operators_stack);
+  stack_pop(operands_stack);
+  stack_pop(operands_stack);
+  stack_push(operands_stack, result.content);
   string_free(&result);
 }
 
@@ -234,4 +304,12 @@ void divide(const char *first_operand, const char *second_operand, string *resul
   size_t result_length = strlen(first_operand) + strlen(second_operand) + 1;
   string_grow_to(result, result_length);
   snprintf(result->content, result_length, "%d", atoi(first_operand) / atoi(second_operand));
+}
+
+char *strdup(const char *s) {
+  size_t len = strlen(s) + 1;
+  void *new = malloc(len);
+  if (new == NULL)
+    return NULL;
+  return (char *) memcpy(new, s, len);
 }
